211026cb4b2ccdf5d69971eab7efb7472ef3c39e
wanglin <wanglin.work@foxmail.com>
2024-01-12 01:06:32 +0800

    修改页面

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,12 +2,21 @@ import { RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import AdminView from "@/views/AdminView.vue";
 import NoAuthView from "@/views/NoAuthView.vue";
+import ACCESS_ENUM from "@/access/accessEnum";
 
 export const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
+    name: "浏览题目",
     component: HomeView,
+  },
+  {
+    path: "/hide",
+    name: "隐藏页面",
+    component: HomeView,
+    meta: {
+      hideInMenu: true,
+    },
   },
   {
     path: "/noAuth",
@@ -19,12 +28,12 @@ export const routes: Array<RouteRecordRaw> = [
     name: "管理员可见",
     component: AdminView,
     meta: {
-      access: "canAdmin",
+      access: ACCESS_ENUM.ADMIN,
     },
   },
   {
     path: "/about",
-    name: "about",
+    name: "关于我的",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
